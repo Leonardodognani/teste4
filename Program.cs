94751a0474a0804fa7b0019111238f8cbdf65361
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-01 14:55:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Lucifer", "Diabolus", "Marchosias", "Astaroth", "Satanas", "Beelzeboth", "Asmodeus", "Lilith", "Leonardo", "Behemoth" };
+        
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+        foreach(string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+
